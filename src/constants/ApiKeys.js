cd602cf14a477fed28bae23c9c7b753f84503e25
QuelleSup <jakub.piotrowski@ig.com>
2019-05-24 15:54:22 +0200

    Update firebase properties file

--- src/constants/ApiKeys.js
+++ src/constants/ApiKeys.js
@@ -1,11 +1,11 @@
 export default {
     FirebaseConfig: {
-        apiKey: "AIzaSyDTPOY8sQi2f4YcNp-jUIxFceqZde30pV0",
-        authDomain: "test-project-19e7b.firebaseapp.com",
-        databaseURL: "https://test-project-19e7b.firebaseio.com",
-        projectId: "test-project-19e7b",
-        storageBucket: "test-project-19e7b.appspot.com",
-        messagingSenderId: "1063448913504",
-        appId: "1:1063448913504:web:b2cfd5d85e263635"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
     }
 }
