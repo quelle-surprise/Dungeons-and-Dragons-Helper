6a352c05079275de56484dee7b24904ae2021e8a
SiRazoR <torecz@o2.pl>
2019-05-29 21:26:57 +0200

    Added QR code functionality

--- src/constants/ApiKeys.js
+++ src/constants/ApiKeys.js
@@ -1,11 +1,11 @@
 export default {
     FirebaseConfig: {
-        apiKey: "",
-        authDomain: "",
-        databaseURL: "",
-        projectId: "",
-        storageBucket: "",
-        messagingSenderId: "",
-        appId: ""
+        apiKey: "AIzaSyBXNhZYVrMpnYLIyRsk1-vu8XbINUDjANA",
+        authDomain: "pomocnik-dnd.firebaseapp.com",
+        databaseURL: "https://pomocnik-dnd.firebaseio.com",
+        projectId: "pomocnik-dnd",
+        storageBucket: "pomocnik-dnd.appspot.com",
+        messagingSenderId: "188196098388",
+        appId: "1:188196098388:web:5b62f3af0f9fef74"
     }
 }
