f1623a4958a633d565095451c764df7b60bdf4eb
Joanna Ulman <joasia.ulman@gmail.com>
2019-06-02 00:34:10 +0200

    added qr-scan icon

--- src/assets/bottomTabNavigator/bottomTabNavigatorIcons.js
+++ src/assets/bottomTabNavigator/bottomTabNavigatorIcons.js
@@ -3,7 +3,8 @@ const bottomTabNavigatorIcons = {
     dice: require("./icons/dice.png"),
     monsters: require("./icons/monsters.png"),
     skills: require("./icons/skills.png"),
-    spells: require("./icons/spells.png")
+    spells: require("./icons/spells.png"),
+    qrScan: require("./icons/qr-scan.png")
 };
 
 export default bottomTabNavigatorIcons;
